7ae16640b8e8fed4142014399886fed8450e6d8f
cph-jn362 <71628258+cph-jn362@users.noreply.github.com>
2024-01-08 08:42:59 +0100

    Add files via upload

---
+++ src/interfaces/book.interface.ts
@@ -0,0 +1,10 @@
+export interface Book {
+    id: number;
+    filename: string;
+    title: string;
+    description: string;
+    price: string;
+    type: string;
+    edition: string;
+    discount: string;
+  }
